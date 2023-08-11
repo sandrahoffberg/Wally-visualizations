263b2b3d76c672bd1249b782349d39c85487badf
Sandra Hoffberg <sandra@codeocean.com>
2023-08-11 13:07:52 +0000

    Edited wally.sh, Dockerfile, postInstall, metadata.yml

--- code/wally.sh
+++ code/wally.sh
@@ -1,9 +1,11 @@
+#!/usr/bin/env bash
+
 source ./config.sh
 
 attached_bams=$(find -L ../data ../scratch -name "*.bam")
 
 
-/wally/src/wally region -r ${chromosome}:${start_position}-${end_position}:../results/${start_position}_${end_position}_${file_name} ${min_read_map_quality_discovery} ${BED_file} ${paired_end_view} ${supplementary_alignments} ${show_clips} ${num_horizontal_images} ${plot_width} ${plot_height} -g ${reference} ${attached_bams}
+/opt/wally/src/wally region -r ${chromosome}:${start_position}-${end_position}:../results/${start_position}_${end_position}_${file_name} ${min_read_map_quality_discovery} ${BED_file} ${paired_end_view} ${supplementary_alignments} ${show_clips} ${num_horizontal_images} ${plot_width} ${plot_height} -g ${reference} ${attached_bams}
 
 
 
